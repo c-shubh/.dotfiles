9fe104a7f1840ed587c67b5b3f75ebc61ed0adf6
Shubh A Chudasama <shubh.cs@proton.me>
2026-02-12 19:15:51 +0530

    Use consult as goto, cycle list, reorder states

--- dot_config/emacs/personal/config.el
+++ dot_config/emacs/personal/config.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
-(setq display-line-numbers-grow-only t)
+(setq-default display-line-numbers-grow-only t)
 
 (defvar my/org-main-file "~/Documents/life.org"
   "The absolute path to my main org file.")
@@ -21,7 +21,9 @@
   :ensure t
 
   :bind (("C-c c" . org-capture)
-         ("C-c a" . org-agenda))
+         ("C-c a" . org-agenda)
+         :map org-mode-map
+         ("C-c C-j" . consult-org-heading))
 
   :hook
   ((org-mode . org-indent-mode)
@@ -32,6 +34,11 @@
   (setq org-default-notes-file my/org-main-file)
   (setq org-agenda-files (list my/org-main-file))
 
+  ;; --- LIST BEHAVIOR ---
+  ;; Treat lists like sub-headlines when cycling visibility
+  (setq org-cycle-include-plain-lists 'integrate)
+
+  ;; --- APPEARANCE ---
   (setq org-hide-leading-stars t)
   (setq org-todo-keyword-faces
         '(("TODO"      . "red")
@@ -41,21 +48,25 @@
           ("DONE"      . "forest green")
           ("CANCELLED" . "saddle brown")))
 
+  ;; --- TODO FLOW ---
   (setq org-enforce-todo-dependencies t)
   (setq org-log-done nil)
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "SOME(s)" "|" "DONE(d)" "CANCELLED(c)")))
+        '((sequence "NEXT(n)" "TODO(t)" "WAITING(w)" "SOME(s)" "|" "CANCELLED(c)" "DONE(d)")))
 
+  ;; --- REFILING ---
   (setq org-refile-targets
         '((nil :maxlevel . 6)
           (org-agenda-files :maxlevel . 6)))
   (setq org-refile-use-outline-path 'file)
   (setq org-outline-path-complete-in-steps nil)
 
+  ;; --- EXPORT ---
   (setq org-export-with-properties t
         org-export-with-tasks t
         org-export-with-planning t)
 
+  ;; --- CAPTURE ---
   (setq org-capture-templates
         '(("t" "Todo" entry (file+headline my/org-main-file "Inbox")
            "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:"
